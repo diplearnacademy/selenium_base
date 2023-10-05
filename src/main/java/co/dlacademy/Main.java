a2a2c8d06821ca7a792834491ba995db067e2967
Bryan Estefan Moreno Diaz <estefandx@hotmail.com>
2023-10-05 17:21:36 -0500

    test project

--- src/main/java/co/dlacademy/Main.java
+++ src/main/java/co/dlacademy/Main.java
@@ -2,6 +2,6 @@ package co.dlacademy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" test Hello world!");
     }
 }
